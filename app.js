0ccd72c2a28fac5b6c485dcc05fb6f1fa3f7082d
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2021-09-02 23:42:25 +0200

    Getting input from user

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const getNotes = require('./notes');
-console.log(getNotes())
+const chalk = require('chalk');
+
+const command = process.argv[2];
+
+if (command === 'add') {
+    console.log('Adding note...')
+} else if (command === 'remove') {
+    console.log('Removing note...');
+} 
